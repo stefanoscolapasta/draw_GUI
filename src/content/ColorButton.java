dc2fea102461cfca764d4f4257a1c091ea18f44f
StefanoScolari <stefano.scolari@studio.unibo.it>
2020-11-14 18:59:10 +0100

    small fix

--- src/content/ColorButton.java
+++ src/content/ColorButton.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
 import javax.swing.JButton;
 
 public class ColorButton extends JButton {
